ea53e91e09821e4e59211b17f5ca36d7d49bed72
Anon Ray <rayanon004@gmail.com>
2020-01-21 12:53:24 +0530

    add IsString instance to GQLQueryText
      with this we can write graphql queries using a quasi-quoter, which
      helps with testing in Pro

--- server/src-lib/Hasura/GraphQL/Transport/HTTP/Protocol.hs
+++ server/src-lib/Hasura/GraphQL/Transport/HTTP/Protocol.hs
@@ -67,7 +67,7 @@ instance (Hashable a) => Hashable (GQLReq a)
 newtype GQLQueryText
   = GQLQueryText
   { _unGQLQueryText :: Text
-  } deriving (Show, Eq, Ord, J.FromJSON, J.ToJSON, Hashable)
+  } deriving (Show, Eq, Ord, J.FromJSON, J.ToJSON, Hashable, IsString)
 
 type GQLReqUnparsed = GQLReq GQLQueryText
 type GQLReqParsed = GQLReq GQLExecDoc
